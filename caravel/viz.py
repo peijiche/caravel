63b927793439b505bb72dbe3bd378c80ecb5aa39
George Ke <georgekeee@gmail.com>
2016-06-13 12:39:49 -0700

    serve mapbox apikey for slice

--- caravel/viz.py
+++ caravel/viz.py
@@ -1744,6 +1744,7 @@ class MapboxViz(BaseViz):
             "customMetric": len(all_columns) >= 1,
             "clusteringRadius": fd.get("clustering_radius"),
             "pointRadiusUnit": fd.get("point_radius_unit"),
+            "mapboxApiKey": config.get('MAPBOX_API_KEY'),
         }
 
 
